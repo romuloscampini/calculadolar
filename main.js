635f4424eb7af23bf7e6da17653ec5be274ae9fe
Romulo Scampini <romulo@scampini.com.br>
2019-08-12 23:22:20 -0300

    impr: get dollar quotation only from week days (because BC only provide data from week days)

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 (function($) {
     $("#calculate-btn").click(function(){
         $.LoadingOverlay("show");
-        var today = moment().subtract('1', 'days').format('MM-DD-YYYY');
+        var i = 1;
+        var today;
+        do{
+            var yesterdayIsWeekend = moment().subtract(i,'days')._d.getDay()%6==0;
+            today = moment().subtract(i,'days').format('MM-DD-YYYY');
+            i++;
+        }while(yesterdayIsWeekend);
         var url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='"+today+"'&$top=100&$format=json&$select=cotacaoVenda";
         $.getJSON( url, function( data ) {
             $.LoadingOverlay("hide");
@@ -12,7 +18,7 @@
                 var sum3 = sum1 + sum2;
                 var sum4 = (sum3/100) * 6.38;
                 var sum5 = sum4 + sum3;
-                var total = sum5.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'})
+                var total = sum5.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})
                 $('#total').val(total);
             } else {
                 bootbox.alert("Cotação do dólar comercial não disponibilizada pelo BCB para o dia de hoje.");
